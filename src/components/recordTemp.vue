063dcfd6f10b89cb59ab38ee42f5fd5401cca5c4
tempestif <soccer.i_y@icloud.com>
2023-08-20 05:21:40 +0900

    連打対応。 sendする関数をPromiseで返却するようにして、同期的に処理。 disabledでボタンを非活性

--- src/components/recordTemp.vue
+++ src/components/recordTemp.vue
@@ -1,14 +1,28 @@
 <script setup lang="ts">
 import { ref } from 'vue'
+// import type { Ref } from 'vue'
 import { sendTemp } from '@/lib/sheetapi'
 
 // 体温
+// ※ここの型、要検討。Ref<number>だと初期値が必要、それ以外は整合性がぱっとみ取れなさそう。
 const temp = ref()
 
-const submit = () => {
-    const today = new Date()
+// ボタンが活性化しているかどうか
+const isEnabled = ref(true)
+
+const submit = async() => {
+    // 連打防止のためにボタンを非活性
+    isEnabled.value = false
+
     // APIを実行
-    sendTemp(today, temp.value)
+    const today = new Date()
+    const res = await sendTemp(today, temp.value)
+
+    // 結果をアラート
+    // ※もうちょいどうにかしたいけど、iOS版Chromeのアラートの質に勝てないならこのままが丸そう。
+    alert(JSON.stringify(res))
+    // ボタンを活性化
+    isEnabled.value = true
 }
 </script>
 
@@ -17,9 +31,7 @@ const submit = () => {
         <label for="temp">体温</label>
         <input type="number" name="temp" v-model="temp">
     </div>
-    <button class="submitBtn" @click="submit">登録</button>
+    <button class="submitBtn" @click="submit" :disabled="!isEnabled">登録</button>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
