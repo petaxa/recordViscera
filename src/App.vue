f6379cc69cabe3c403d6665d55973de09f6c9dad
tempestif <soccer.i_y@icloud.com>
2023-08-21 21:16:07 +0900

    App.vueにあったグローバルにあるCSSをmain.cssに移動 base.cssに--main-colorを追加

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,12 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
 </script>
-
 <template>
   <RouterView />
 </template>
-
 <style>
-input {
-  font-size: 16px;
-}
-
 /* 共通のCSS */
+/* NOTE: どうにかしてrecordPoo, recordTempだけでしか使えないようにしたい。二回書くのは論外。うーん。 */
 .inputForm {
   margin: 10px 0;
   width: 100%;
@@ -24,10 +19,6 @@ input {
 .submitBtn {
   width: 100%;
   height: 40px;
-  /* border-color: white; */
-  background-color: transparent;
-  border-radius: 5px;
-  /* color:white; */
   font-size: 20px;
   margin-top: 30px;
 }
